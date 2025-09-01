6347215d597300c231fd636d6a70405fbe8ea39c
Harrison King <harrisonking3465@gmail.com>
2025-09-01 21:20:36 +0100

    setting up the information about the project

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello, GitHub!")
+"""
+Advanced ML-Enhanced Test Failure Prediction Framework
+Research-Grade Prototype for University of Tokyo Masters Application
+
+Author: Harrison King
+Institution: University of Kent Canterbury
+Research Area: Leveraging Machine Learning to Improve Automated Software Testing
+
+"""
